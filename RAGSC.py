5432a9e15f5c0e54d15a8d029b762bbef1133811
LiminLi-xjtu <59948715+LiminLi-xjtu@users.noreply.github.com>
2024-12-16 22:29:18 +0800

    Add files via upload

--- RAGSC.py
+++ RAGSC.py
@@ -2,122 +2,25 @@ import numpy as np
 import scipy.io as sio
 import matplotlib.pyplot as plt
 from model import RAG
-
+from utiles import dataset_params, load_dataset
 
 
 if __name__ == '__main__':
-    # Load data
-    dataset = 'citeseer'
-    gamma = 0.001
+    dataset = 'citeseer'  #"cora", "citeseer", "wiki", "acm", "dblp", "cora_sorted" or "citeseer_sorted"
+    X, A, gnd = load_dataset(dataset)
+    params = dataset_params[dataset]
+    print('Run dataset "{}"'.format(dataset))
 
-
-    if dataset == 'cora':
-        data = sio.loadmat('data/{}.mat'.format(dataset))
-        X = data['fea']
-        A = data['W']
-        gnd = data['gnd']
-        gnd = gnd.T
-        gnd = gnd - 1
-        gnd = gnd[0, :]
-        k = 7
-        Z, acc, nmi, F1 = RAG(A, X, gnd, k, eta=300000, n_class=k, alpha=120, beta=1, lamb=0.5, t=16, tau=0.005,
-                              max_iter=30)
-
-        plt.figure(figsize=(8, 6))
-        plt.xlabel('Iteration (Cora)', fontsize=24)
-        plt.ylabel('Accuracy & NMI & F1', fontsize=24)
-        # plt.ylim((0.5, 1))
-        plt.plot(acc, color='red', label='Accuracy', linewidth=4)
-        plt.plot(nmi, color='green', label='NMI', linewidth=4)
-        plt.plot(F1, color='blue', label='F1', linewidth=4)
-        plt.xticks(fontsize=22)
-        plt.yticks(fontsize=22)
-        plt.legend(fontsize=22)
-        plt.tight_layout()
-        plt.show()
-
-
-
-    if dataset == 'citeseer':
-        data = sio.loadmat('data/{}.mat'.format(dataset))
-        X = data['fea']
-        A = data['W']
-        gnd = data['gnd']
-        gnd = gnd.T
-        gnd = gnd - 1
-        gnd = gnd[0, :]
-        k = 6
-        Z, acc, nmi, F1 = RAG(A, X, gnd, k, eta=600000, n_class=k, alpha=41, beta=0.01, lamb=0, t=8,
-                              tau=0.005, max_iter=30)
-
-
-    if dataset == 'wiki':
-        data = sio.loadmat('data/{}.mat'.format(dataset))
-        k = 17
-        X = data['fea']
-        A = data['W']
-        gnd = data['gnd']
-        gnd = gnd.T
-        gnd = gnd - 1
-        gnd = gnd[0, :]
-        Z, acc, nmi, F1 = RAG(A, X, gnd, k, eta=600000, n_class=k, alpha=150, beta=1, lamb=3, t=4, tau=0.005,
-                              max_iter=100)
-        # max_iter=63,eta=600000, theta=1, alpha=150, beta=1, lamb=3,t=4
-
-    if (dataset == 'acm'):
-        data = sio.loadmat('data/{}.mat'.format(dataset))
-        k = 3
-        X = data['fea']
-        A = data['W']
-        gnd = data['gnd'][0, :]
-        Z, acc, nmi, F1 = RAG(A, X, gnd, k, eta=2800000, n_class=k, alpha=200, beta=1, lamb=0, t=5, tau=0.005,
-                              max_iter=30)
-        # maxiter=21 , eta=2800000, theta=1, alpha=200, beta=1, lamb=0,t=5
-
-    if (dataset == 'dblp'):
-        data = sio.loadmat('data/{}.mat'.format(dataset))
-        k = 4
-        X = data['fea']
-        A = data['W']
-        gnd = data['gnd'][0, :]
-        Z, acc, nmi, F1 = RAG(A, X, gnd, k, eta=1000000, n_class=k, alpha=50, beta=0.5, lamb=0.1, t=1,
-                              tau=0.005, max_iter=10)
-        # maxiter=5, eta=1000000, theta=1, alpha=50, beta=0.5, lamb=0.1, t=1
-
-
-
-
-
-# cora-sorted version, conveniently used to visualize matrix block diagonal effects.
-    sort = False
-    if dataset == 'cora' and sort == True:
-        features_struct = np.load('data/cora_sorted.npz')
-        X = features_struct['features']
-        gnd = features_struct['labels']
-        A = features_struct['adjacency']
-        k = 7
-        gnd = gnd - 1
-        Z, acc, nmi, F1 = RAG(A, X, gnd, k, eta=300000, n_class=k, alpha=115, beta=1, lamb=0.5, t=16, tau=0.005,
-                              max_iter=30)  # max_iter=7,eta=300000, theta=1, alpha=115, beta=1, lamb=0.5, t=16
-
-
-    if dataset == 'citeseer'and sort == True:
-        features_struct = np.load('data/citeseer_sorted.npz')
-        X = features_struct['features']
-        gnd = features_struct['labels']
-        A = features_struct['adjacency']
-        k = 6
-        gnd = gnd - 1
-        Z, acc, nmi, F1 = RAG(A, X, gnd, k, eta=600000, n_class=k, alpha=42, beta=0.0125, lamb=0, t=8,
-                              tau=0.005, max_iter=50)
-        # max_iter=1,eta=600000,theta=1, alpha=80, beta=100, lamb=1,k=8
-
-
-
-
-
-
-
-
-
+    # run RAG
+    Z, acc, nmi, F1 = RAG(
+        A,
+        X,
+        gnd,
+        eta=params["eta"],
+        alpha=params["alpha"],
+        beta=params["beta"],
+        lamb=params["lamb"],
+        t=params["t"],
+        max_iter=params["max_iter"],
+    )
 
--- model.py
+++ model.py
@@ -4,9 +4,10 @@ import scipy.sparse as sp
 from scipy.sparse import eye, issparse, triu  # Import the eye function from the SciPy library for creating sparse identity matrices
 from scipy.sparse.linalg import eigs  # Import the eigs function from the SciPy library for computing eigenvalues and eigenvectors of sparse matrices
 import scipy.io as sio
-from utiles import clustering_metrics
+from utiles import clustering_metrics, update_local_best, filter_features_sparse, spectral_clustering
 from scipy import sparse
 from sklearn.cluster import KMeans
+from sklearn.cluster import SpectralClustering
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 from scipy.sparse import csr_matrix
@@ -15,76 +16,57 @@ import time
 
 
 
-def RAG(A, X, gnd, k, eta, n_class, alpha, beta, lamb, t, tau, max_iter=10):
+def RAG(A, X, gnd, eta, alpha, beta, lamb, t, max_iter=10):
     """
     Attributed Graph Refinement via low rank approximation and subspace learning (RAG)
     """
     gamma = 0.001
     n = A.shape[0]
-    r_relax = 0
+    k = len(np.unique(gnd))
 
     if isinstance(A, np.ndarray):
         Anorm = np.linalg.norm(A, ord=None)
     else:
         Anorm = np.linalg.norm(A.A, ord=None)
 
-    scale = np.sqrt(n_class) / Anorm # Compute a scaling factor based on the number of classes and the Frobenius norm of A
-    As = (A + A.T) * (scale / 2)  # Scale and symmetrize the input matrix A
+    scale = np.sqrt(k) / Anorm
+    As = (A + A.T) * (scale / 2)
+
 
     # filter X
     I = np.eye(n)
-    A = A + I
-    D = np.sum(A, axis=1)
-    D = np.power(D, -0.5)
-    D[np.isinf(D)] = 0
-    D = np.diagflat(D)
-    D_sparse = csr_matrix(D)
-    A_sparse = csr_matrix(A)
-    A = A_sparse.dot(D)
-    A = D_sparse.dot(A)
-
-    # Get filter G
-    Ls = I - A
-    G = I - 0.5 * Ls
-    G_sparse = csr_matrix(G)
-
-    for i in range(t):
-        X = G_sparse.dot(X)
+    X = filter_features_sparse(A, X, t)
     if issparse(X):
         X = X.todense()
 
     Xnorm = np.linalg.norm(X, ord=None)
-    scaleX = np.sqrt(n_class) / Xnorm
+    scaleX = np.sqrt(n) / Xnorm
     X = X * scaleX
-
     alpha_XX = alpha * np.dot(X, X.T)
 
 
     Z = 0
     Y = As
-
     acc = []
     nmi = []
     F1 = []
 
-    u, s, v = sp.linalg.svds(Y, k=k, which='LM')
-    u = u.astype(float)
-    kmeans = KMeans(n_clusters=k, random_state=23).fit(u)
-    predict_labels = kmeans.predict(u)
-    cm = clustering_metrics(gnd, predict_labels)
+    y_A = SpectralClustering(n_clusters=k, assign_labels="discretize", random_state=23, affinity='precomputed').fit(As).labels_
+    cm = clustering_metrics(gnd, y_A)
     ac, nm, f1 = cm.evaluationClusterModelFromLabel()
     acc.append(ac)
-    print('ORIGINAL A : acc_mean: {:.4f}'.format(ac),
-        'nmi_mean: {:.4f}'.format(nm),
-        'f1_mean: {:.4f}'.format(f1))
+    # print('ORIGINAL A : acc_mean: {:.4f}'.format(ac),
+    #     'nmi_mean: {:.4f}'.format(nm),
+    #     'f1_mean: {:.4f}'.format(f1))
 
     local_best_acc = 0.0
     local_best_nmi = 0.0
     local_best_f1 = 0.0
     local_best_iter = 0
 
-    err = 1  # Initialize the error to be greater than the convergence tolerance
-    iter = 0  # Initialize the iteration counter
+    err = 1
+    iter = 0
+    tau = 0.005
 
     while err > tau and iter < max_iter:  # While the error is greater than the convergence tolerance and the maximum number of iterations has not been reached,
         iter += 1
@@ -93,7 +75,7 @@ def RAG(A, X, gnd, k, eta, n_class, alpha, beta, lamb, t, tau, max_iter=10):
         start_time = time.time()
 
         # Update U
-        U = eigs(Y, k=n_class + r_relax, which='LM')[1]  # compute the eigenvectors of Z corresponding to its largest eigenvalues
+        U = eigs(Y, k=k, which='LM')[1]  # compute the eigenvectors of Z corresponding to its largest eigenvalues
         UU = np.real(np.dot(U, U.T))
 
         # Update Z
@@ -102,46 +84,31 @@ def RAG(A, X, gnd, k, eta, n_class, alpha, beta, lamb, t, tau, max_iter=10):
         Y = linalg.solve(AAA, BBB)
         Y = Y.astype(float)
 
-        D = Y * (Y > 0)  # compute a matrix D based on Z
+        D = Y * (Y > 0)
 
         # Update Z to T_eta(Z,D) by truncating Z:
-        Z = shrink(Y, D, eta, n)  # apply a truncation operation to Z using the helper function shrink
+        Z = shrink(Y, D, eta, n)
 
-
-        err = np.linalg.norm(Z - Z_old, ord=None)  # compute the change in Z between iterations using the Frobenius norm
+        err = np.linalg.norm(Z - Z_old, ord=None)
         err = err / np.linalg.norm(Z, ord=None)
 
         end_time = time.time()
         print(f"Time spent in iteration {iter}: {round(end_time - start_time, 4)} s")
 
         Z_sym = (np.abs(Z) + np.abs(Z.T)) / 2
-
-        u, s, v = sp.linalg.svds(Z_sym, k=k, which='LM')
-        u = u.astype(float)
-        kmeans = KMeans(n_clusters=k, random_state=23).fit(u)
-        predict_labels = kmeans.predict(u)
-        # cmm = confusion_matrix(gnd, predict_labels)
+        predict_labels = spectral_clustering(Z_sym, k=k, random_state=23)
         cm = clustering_metrics(gnd, predict_labels)
         ac, nm, f1 = cm.evaluationClusterModelFromLabel()
         acc.append(ac)
         nmi.append(nm)
         F1.append(f1)
 
-        if err < 0.05 and ac > local_best_acc:
-            local_best_acc = acc[iter - 1]
-            local_best_nmi = nmi[iter - 1]
-            local_best_f1 = F1[iter - 1]
-            local_best_iter = iter - 1
-
-        if err_old < err and (acc[iter - 1] + nmi[iter - 1] + F1[iter - 1] > local_best_acc + local_best_nmi + local_best_f1):
-            local_best_acc = acc[iter - 1]
-            local_best_nmi = nmi[iter - 1]
-            local_best_f1 = F1[iter - 1]
-            local_best_iter = iter - 1
-
+        local_best_acc, local_best_nmi, local_best_f1, local_best_iter = update_local_best(
+            err, err_old, acc, nmi, F1, iter,
+            local_best_acc, local_best_nmi, local_best_f1, local_best_iter,
+            max_iter, tau)
         if iter == max_iter or err < tau:
-            print('local_best_iter:{}'.format(local_best_iter),
-                  'local_best_acc: {:.4f}'.format(local_best_acc),
+            print('local_best_acc: {:.4f}'.format(local_best_acc),
                   'local_best_nmi: {:.4f}'.format(local_best_nmi),
                   'local_best_f1: {:.4f}'.format(local_best_f1))
     return Z, acc, nmi, F1
@@ -156,5 +123,3 @@ def shrink(Z, D, eta, n):  # Define a helper function for truncating a matrix
     dd = dd[::-1]  # Reverse dd to obtain it in descending order
     entry = ((eye(n) + (D >= dd[eta_1])) != 0)
     return Z * entry
-
-
